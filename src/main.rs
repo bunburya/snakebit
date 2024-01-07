6da164fe25b970c482819de2261ce4f35485458a
bunburya <dev@bunburya.eu>
2024-01-07 00:28:29 +0000

    adding progressing speed increase

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod sound;
 
 use cortex_m_rt::entry;
 use microbit::Board;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::rtt_init_print;
 use microbit::hal::{Rng, Timer};
 use microbit::display::blocking::Display;
 use microbit::hal::prelude::*;
@@ -29,23 +29,21 @@ fn main() -> ! {
     init_buttons(board.GPIOTE, board.buttons);
 
     let mut display = Display::new(board.display_pins);
-    //let mut display = microbit::display::nonblocking::Display::new(
-    //    board.TIMER1,
-    //    board.display_pins
-    //);
 
     loop {
         loop {  // Game loop
-            let image = game.display(8, 4, 2);
-            display.show(&mut timer, image, 1000);
+            let image = game.game_matrix(8, 4, 2);
+            display.show(&mut timer, image, game.step_len_ms());
             match game.status {
                 GameStatus::Ongoing => game.step(get_turn(true)),
                 _ => {
                     for _ in 0..3 {
                         display.clear();
                         timer.delay_ms(200u32);
-                        display.show(&mut timer, image, 200u32);
+                        display.show(&mut timer, image, 200);
                     }
+                    display.clear();
+                    display.show(&mut timer, game.score_matrix(9), 1000);
                     break
                 }
             }
